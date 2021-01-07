8acc300131c4fb12bc45ce917b52facc249c176a
Lamant <uyti0713@gmail.com>
2021-01-08 03:07:36 +0900

    guestbook2 - controller test

---
+++ src/com/javaex/controller/GuestController.java
@@ -0,0 +1,23 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/gbc")
+public class GuestController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//컨트롤러 테스트
+   		System.out.println("Controller"); //ok
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
